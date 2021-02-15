98427127be709631e71ea3c6eb5d7ae1b0ac455d
leesy744 <78004293+leesy744@users.noreply.github.com>
2021-02-16 07:46:37 +0900

    Add files via upload

---
+++ 0_semiProject/src/com/recipe/mBoardSearch.java
@@ -0,0 +1,61 @@
+package com.recipe.mBoard.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.recipe.mBoard.service.mBoardService;
+import com.recipe.mBoard.model.mBoardVO;
+
+@WebServlet("/mBoardSearch.do")
+public class mBoardSearch extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		execute(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		execute(request, response);
+	}
+	protected void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		
+		String pageNum = request.getParameter("pageNum");
+		String keyword = request.getParameter("keyword");
+
+		int pageSize = 10;
+		
+
+		if (pageNum == null) {
+			pageNum = "1";
+		}
+		int currentPage = Integer.parseInt(pageNum);
+
+		int startRow = (currentPage - 1) * pageSize + 1;
+		int endRow = currentPage * pageSize;
+		
+
+		int count = 0;
+		int number = 0;
+		
+		count = new mBoardService().getSearchBoardCount(keyword);
+		ArrayList<mBoardVO> list = new mBoardService().getSearchBoard(keyword,startRow, endRow);
+		number = count - (currentPage - 1) * pageSize;
+		
+		request.setAttribute("list", list);
+		request.setAttribute("number", number);
+		request.setAttribute("pageSize", pageSize);
+		request.setAttribute("count", count);
+		request.setAttribute("currentPage", currentPage);
+
+		request.getRequestDispatcher("/mBoard/boardAllList.jsp").forward(request, response);
+		
+	}
+
+}
---
+++ 0_semiProject/src/com/recipe/mBoardUpdate.java
@@ -0,0 +1,44 @@
+package com.recipe.mBoard.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.recipe.mBoard.service.mBoardService;
+import com.recipe.mBoard.model.mBoardVO;
+
+@WebServlet("/mBoardUpdate.do")
+public class mBoardUpdate extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		execute(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		execute(request, response);
+	}
+	protected void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		request.setCharacterEncoding("UTF-8");
+
+		int num = Integer.parseInt(request.getParameter("num"));
+//		String password = request.getParameter("password");
+//		String pass = request.getParameter("pass");
+		String title = request.getParameter("title");
+		String content = request.getParameter("content");
+		int result = new mBoardService().updateBoard(num, title, content);
+		
+		if(result > 0) {
+			mBoardService ms = new mBoardService();
+			ms.updateBoard(num, title, content);
+		
+			RequestDispatcher dis = request.getRequestDispatcher("/mBoard/updateSuccess.jsp");
+			request.setAttribute("num", num);
+			dis.forward(request ,response);
+		}
+		
+	}		
+	}
