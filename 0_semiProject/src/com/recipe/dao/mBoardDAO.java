3d82003295eb726c6a4ec5ea0d3c28f9f8dc6edb
leesy744 <78004293+leesy744@users.noreply.github.com>
2021-02-16 07:40:51 +0900

    Update mBoardDAO.java

--- 0_semiProject/src/com/recipe/dao/mBoardDAO.java
+++ 0_semiProject/src/com/recipe/dao/mBoardDAO.java
@@ -22,15 +22,12 @@ public class mBoardDAO {
 	ResultSet rs = null;
 	
 
-	
-	public int getAllCount(Connection conn) {
+public int getAllCount(Connection conn) {
 		int count = 0;
-		
-		try {
-		
+				try {
+//		
 			String sql ="SELECT COUNT(*) FROM M_BOARD";
-			pstmt = conn.prepareStatement(sql);
-			
+			pstmt = conn.prepareStatement(sql);			
 			rs = pstmt.executeQuery();
 			
 			if(rs.next()){
@@ -46,12 +43,12 @@ public class mBoardDAO {
 		return count;
 	}
 	
-	public Vector<mBoardVO> getAllBoard(Connection conn, int startRow, int endRow){
+	public Vector<mBoardVO> getAllBoard(Connection conn, int startRow, int endRow){ //모든게시글리턴
 		Vector<mBoardVO> v = new Vector<>();
 		
 		try {
 			String sql = "SELECT * FROM (SELECT M.*, ROWNUM rnum FROM(SELECT * FROM M_BOARD ORDER BY DESC, ASC)A)"
-					+ "WHERE rnum >= ? AND rnum <= ?";
+					+ "WHERE rnum >= ? AND rnum <= ?";   //
 			
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setInt(1, startRow);
@@ -64,8 +61,8 @@ public class mBoardDAO {
 				vo.setBoardTitle(rs.getString(2));				
 				vo.setBoardContent(rs.getString(3));
 				vo.setBoardWriter(rs.getString(4));
-				vo.setBoardDate(rs.getDate(5).toString());
-				vo.setBoardHit(rs.getString(6));
+				vo.setBoardDate(rs.getDate(5));  //.toString());
+				vo.setBoardHit(rs.getInt(6));
 				
 				
 				
@@ -84,7 +81,7 @@ public class mBoardDAO {
 		mBoardVO vo = new mBoardVO();
 		
 		try {
-			String rsql = "UPDATE M_BOARD SET BOARDHIT = BOARDHIT+1 WHERE BOARD_NUM=?";
+			String rsql = "UPDATE M_BOARD SET BOARD_HIT = BOARD_HIT+1 WHERE BOARD_NUM=?";
 			pstmt = conn.prepareStatement(rsql);
 			pstmt.setInt(1,num);
 			pstmt.executeUpdate();	
@@ -99,8 +96,8 @@ public class mBoardDAO {
 				vo.setBoardTitle(rs.getString(2));				
 				vo.setBoardContent(rs.getString(3));
 				vo.setBoardWriter(rs.getString(4));
-				vo.setBoardDate(rs.getDate(5).toString());
-				vo.setBoardHit(rs.getString(6));
+				vo.setBoardDate(rs.getDate(5));
+				vo.setBoardHit(rs.getInt(6));
 			
 				
 			}
@@ -129,8 +126,8 @@ public class mBoardDAO {
 				vo.setBoardTitle(rs.getString(2));				
 				vo.setBoardContent(rs.getString(3));
 				vo.setBoardWriter(rs.getString(4));
-				vo.setBoardDate(rs.getDate(5).toString());
-				vo.setBoardHit(rs.getString(6));
+				vo.setBoardDate(rs.getDate(5));
+				vo.setBoardHit(rs.getInt(6));
 				
 			}
 		} catch (Exception e) {
@@ -142,14 +139,16 @@ public class mBoardDAO {
 		}
 		return vo;
 	}
-	public int updateBoard(Connection conn,int num ,String title ,String content){
+	public int updateBoard(Connection conn, int num ,String title ,String content){
+		
 		int result = 0;
 		try {		
-			String sql = "UPDATE M_BOARD SET BOARD_NUM=?,BOARD_TITLE=? WHERE BOARD_CONTENT=?";
+			String sql = "UPDATE M_BOARD SET BOARD_TITLE=? WHERE BOARD_NUM=?";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1,num);
-			pstmt.setString(2,title);
-			pstmt.setString(3,content);
+			
+			pstmt.setString(1,title);
+			pstmt.setString(2,content);
+			pstmt.setInt(3, num);
 			result = pstmt.executeUpdate();
 
 		}catch(Exception e) {
@@ -192,14 +191,12 @@ public class mBoardDAO {
 			
 			while(rs.next()) {
 				mBoardVO vo = new mBoardVO();
-				vo.setBoardNUM(rs.getInt(1));
+				vo.setBoardNUM(rs.getInt(1));  //
 				vo.setBoardTitle(rs.getString(2));				
 				vo.setBoardContent(rs.getString(3));
-				vo.setBoardDate(rs.getDate(4).toString());
-				vo.setBoardHit(rs.getString(5));
-				
-				
-				
+				vo.setBoardDate(rs.getDate(4));
+				vo.setBoardHit(rs.getInt(5));
+							
 				list.add(vo);
 			}
 		} catch (Exception e) {
@@ -231,4 +228,27 @@ public class mBoardDAO {
 		}return postCount;
 	}
 	
+	public static int insertBoard(Connection conn, mBoardVO vo) {  // 서비스와 관련 static 변경
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String query = "insert into M_BOARD values(m_board_seq.nextval,?,?,?,sysdate,?)";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, vo.getBoardTitle());
+			pstmt.setString(2, vo.getBoardContent());
+			pstmt.setString(3, vo.getBoardWriter());
+			//pstmt.setDate(4, vo.getBoardDate());
+			pstmt.setInt(4, vo.getBoardHit());
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(pstmt);
+		}
+		return result;
+	}
 }
+	
